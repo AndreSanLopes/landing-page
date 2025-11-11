936ea226f02046070136e1a126eff888176a088a
André Santana <109093157+AndreSanLopes@users.noreply.github.com>
2025-11-11 09:04:41 -0300

    Ajustes finais no Header

---
+++ src/javascript/script.js
@@ -0,0 +1,9 @@
+/*
+CÓDIGO JQUERY
+*/
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+});
