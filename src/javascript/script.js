30984bf03c97916048771afd2ed3c281353640f8
André Santana <109093157+AndreSanLopes@users.noreply.github.com>
2025-11-12 11:44:29 -0300

    Ajuste fino no header e main
    - correção de bugs
    -header funcional
    - botões e listas funcionais
    - separação do menu mobile do elemento <nav> (bug principal)
    - ajustes de responsividade
    ###########
    - main incorporado
    - adição de imagens para o banner
    - seção home incorporada
    - seção menu incorporada
    - imagens editadas

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,9 +1,18 @@
 /*
 CÓDIGO JQUERY
 */
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
-    });
+
+//ANIMAÇÃO DO BOTÃO MOBILE
+$(document).ready(function () {
+  //"OUVE" O CLICK NO BOTÃO
+  $("#mobile_btn").on("click", function () {
+    //ADICIONA A CLASS "active" NO BOTÃO
+    $("#mobile_menu").toggleClass("active");
+    //SELECIONA O ÍCONE DENTRO DO BOTÃO
+    const icon = $(this).find("i");
+
+    icon.toggleClass("fa-bars fa-xmark");
+
+    //$('#mobile_btn').find('i').toggleClass('fa-xmark');
+  });
 });
