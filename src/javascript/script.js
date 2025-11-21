a6f2256dcc3391c16abb4efc420b347ef9fbddca
André Santana <109093157+AndreSanLopes@users.noreply.github.com>
2025-11-21 07:24:16 -0300

    Footer; JS; animações; SEO on page
    - introdução do scroll reveal
    - ajustes no header (finalização)

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -15,4 +15,44 @@ $(document).ready(function () {
 
     //$('#mobile_btn').find('i').toggleClass('fa-xmark');
   });
+  //ANIMAÇÕES DE SCROLLING
+  const sections = $("section");
+  const navItems = $(".nav-item");
+
+  //FUNÇÃO DE SCROLL : ANIMAÇÃO
+  $(window).on("scroll", function () {
+    const header = $("header");
+    //GUARDAR POSIÇÃO DO SCROLL NA JANELA
+    const scrollPosition = $(window).scrollTop() - header.outerHeight(); //ALTURA DO HEADER É SUBTRAÍDA
+    //ANIMAÇÃO SOMBREAMENTO HEADER
+    let activeSectionIndex = 0;
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0,0,0,0.1");
+    }
+    //LOOP
+    sections.each(function (i) {
+      const section = $(this); //GUARDA A SESSÃO
+      const sectionTop = section.offset().top - 93; //RECEBE O TOPO DA SESSÃO - ALTURA DA NAV
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      //VERIFICA SE A POSIÇÃO DA ROLAGEM
+      //ESTÁ NA ÁREA DA SEÇÃO ESPECIFICADA
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false; //SAI DO LOOP
+      }
+    });
+
+    navItems.removeClass("active"); //REMOVE A CLASSE "ACTIVE" DOS MENUS CONFORME O SCROLL
+    $(navItems[activeSectionIndex]).addClass("active"); //TROCA A CLASSE DO MENU PARA "ACTIVE"
+  });
+
+  //ANIMAÇÕES SCROLL REVEAL
+  scrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%"
+  });
 });
